ca11a07c66303ad8664512726f633b5c2b7e693e
DocPillman <doc.pillman@icloud.com>
2020-05-31 20:38:44 +0300

    delete trash

--- drivers/cm4.c
+++ drivers/cm4.c
@@ -1,58 +1,13 @@
 // Includes for the Canfestival driver
-#include "stm32f10x_rcc.h"
-#include "stm32f10x_can.h"
-#include "stm32f10x_gpio.h"
-#include "misc.h"
-#include "stm32f10x_tim.h"
 #include "canfestival.h"
 #include "timer.h"
-#include "cm3.h"
-#include "can.h"
-
-/* Gestion d'un buffer tournant de messages CAN en Tx pour que le transfert
- * en mode block fonctionne avec SDO_BLOCK_SIZE > 3 
- */
-#define MESS_BUFFER_SIZE (SDO_BLOCK_SIZE + 3)
-typedef struct  
-{
-    Message Buf[MESS_BUFFER_SIZE];
-    Message *readp;
-    Message *writep;
-} MessBuf_t;
-static MessBuf_t messBuf;
-void MessBuf_Init(MessBuf_t *pFifo);
-int MessBuf_Write(MessBuf_t *pFifo, Message *pgd);
-int MessBuf_Read(MessBuf_t *pFifo, Message *pgd);
-
-static uint8_t tx_fifo_in_use = 0;
+#include "data.h"
+#include "cm4.h"
 
 TIMEVAL last_counter_val = 0;
 TIMEVAL elapsed_time = 0;
 
 static CO_Data *co_data = NULL;
-
-void clearTimer(void)
-{
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = DISABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
-	/* TIM3 disable counter */
-	TIM_Cmd(TIM3, DISABLE);
-
-	/* TIM Interrupts disable */
-	TIM_ITConfig(TIM3, TIM_IT_Update, DISABLE);
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, DISABLE);
-
-}
-
-void start_callback(CO_Data* d, UNS32 id)
-{
-}
 
 // Initializes the timer, turn on the interrupt and put the interrupt time to zero
 void initTimer(void)
@@ -70,7 +25,7 @@ void initTimer(void)
 	NVIC_Init(&NVIC_InitStructure);
 
 	/* Compute the prescaler value */
-	/* SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f10x.c file */
+	/* SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f37x.c file */
 	uint16_t PrescalerValue = (uint16_t) (SystemCoreClock  / 100000) - 1;
 
 	/* Time base configuration */
@@ -87,13 +42,10 @@ void initTimer(void)
 	TIM_Cmd(TIM3, DISABLE);
 
 	/* Preset counter for a safe start */
-	TIM_SetCounter(TIM3, 0);
+	TIM_SetCounter(TIM3, 1);
 
 	/* TIM Interrupts enable */
 	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
-
-	// this is needed for correct canfestival virtual timer management start
-	SetAlarm(NULL, 0, start_callback, 0, 0);
 }
 
 //Set the timer for the next alarm.
@@ -104,6 +56,7 @@ void setTimer(TIMEVAL value)
 	last_counter_val = 65535-value;
 	TIM_SetCounter(TIM3, 65535-value);
 	TIM_Cmd(TIM3, ENABLE);
+	//printf("setTimer %lu, elapsed %lu\r\n", value, elapsed_time);
 }
 
 //Return the elapsed time to tell the Stack how much time is spent since last call.
@@ -156,44 +109,34 @@ unsigned char canInit(CO_Data * d, uint32_t bitrate)
   NVIC_InitTypeDef  NVIC_InitStructure;
   CAN_InitTypeDef        CAN_InitStructure;
   CAN_FilterInitTypeDef  CAN_FilterInitStructure;
-    
+  
   /* save the canfestival handle */  
   co_data = d;
 
-  MessBuf_Init(&messBuf);
-
   /* CAN GPIOs configuration **************************************************/
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
   /* Enable GPIO clock */
-  RCC_APB2PeriphClockCmd(CAN_GPIO_CLK, ENABLE);
-  
-  /* Configure CAN pin: RX */
-  GPIO_InitStructure.GPIO_Pin = CAN_RX_PIN;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-  GPIO_Init(CAN_GPIO_PORT, &GPIO_InitStructure);
-  
-  /* Configure CAN pin: TX */
-  GPIO_InitStructure.GPIO_Pin = CAN_TX_PIN;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(CAN_GPIO_PORT, &GPIO_InitStructure);
+  RCC_AHBPeriphClockCmd(CAN_GPIO_CLK, ENABLE);
 
-  GPIO_PinRemapConfig(GPIO_Remap1_CAN1 , ENABLE);
+  /* Connect CAN pins to AF7 */
+  GPIO_PinAFConfig(CAN_GPIO_PORT, CAN_RX_SOURCE, CAN_AF_PORT);
+  GPIO_PinAFConfig(CAN_GPIO_PORT, CAN_TX_SOURCE, CAN_AF_PORT); 
+  
+  /* Configure CAN RX and TX pins */
+  GPIO_InitStructure.GPIO_Pin = CAN_RX_PIN | CAN_TX_PIN;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_Init(CAN_GPIO_PORT, &GPIO_InitStructure);
 
   /* NVIC configuration *******************************************************/
-  NVIC_InitStructure.NVIC_IRQChannel = CAN1_TX_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
-  
   NVIC_InitStructure.NVIC_IRQChannel = CAN1_RX0_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
-
+  
   /* CAN configuration ********************************************************/  
   /* Enable CAN clock */
   RCC_APB1PeriphClockCmd(CAN_CLK, ENABLE);
@@ -204,12 +147,13 @@ unsigned char canInit(CO_Data * d, uint32_t bitrate)
 
   /* CAN cell init */
   CAN_InitStructure.CAN_TTCM = DISABLE;
-  CAN_InitStructure.CAN_ABOM = ENABLE;
+  CAN_InitStructure.CAN_ABOM = DISABLE;
   CAN_InitStructure.CAN_AWUM = DISABLE;
   CAN_InitStructure.CAN_NART = DISABLE;
   CAN_InitStructure.CAN_RFLM = DISABLE;
-  CAN_InitStructure.CAN_TXFP = ENABLE;
+  CAN_InitStructure.CAN_TXFP = DISABLE;
   CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;
+  CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;
     
   /* CAN Baudrate (CAN clocked at 36 MHz)  36e6 / ( prescaler * (1+BS1+BS2))  */
   CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;
@@ -226,7 +170,6 @@ unsigned char canInit(CO_Data * d, uint32_t bitrate)
   	CAN_InitStructure.CAN_BS2 = CAN_BS2_2tq;
   }
   CAN_InitStructure.CAN_Prescaler = brp_from_birate(bitrate);
-  
   CAN_Init(CANx, &CAN_InitStructure);
 
   /* CAN filter init */
@@ -247,23 +190,8 @@ unsigned char canInit(CO_Data * d, uint32_t bitrate)
   return 1;
 }
 
-void canClose(void)
-{
-  NVIC_InitTypeDef  NVIC_InitStructure;
-  /* NVIC configuration : remove irq */
-  NVIC_InitStructure.NVIC_IRQChannel = CAN1_TX_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = DISABLE;
-  NVIC_Init(&NVIC_InitStructure);
-  NVIC_InitStructure.NVIC_IRQChannel = CAN1_RX0_IRQn;
-  NVIC_Init(&NVIC_InitStructure);
-  /* CAN déinit */
-  CAN_DeInit(CANx);
-  /* disable CAN clock */
-  RCC_APB1PeriphClockCmd(CAN_CLK, DISABLE);
-}
-
 // The driver send a CAN message passed from the CANopen stack
-unsigned char lowlevel_canSend(Message *m)
+unsigned char canSend(CAN_PORT notused, Message *m)
 {
 	int i, res;
 	CanTxMsg TxMessage = {0};
@@ -282,37 +210,6 @@ unsigned char lowlevel_canSend(Message *m)
     return 1;		// succesful
 }
 
-void USB_HP_CAN1_TX_IRQHandler(void)
-{
-	Message msg;
-	CAN_ClearITPendingBit(CAN1, CAN_IT_TME);
-	if(MessBuf_Read(&messBuf, &msg)){
-  		CAN_ITConfig(CANx, CAN_IT_TME, DISABLE);	// fifo vide, pas de trames à envoyer
-		tx_fifo_in_use = 0;
-	}
-	else
-		lowlevel_canSend(&msg);
-}
-
-// The driver send a CAN message passed from the CANopen stack
-unsigned char canSend(CAN_PORT notused, Message *m)
-{
-	unsigned char rv;
-  	CAN_ITConfig(CANx, CAN_IT_TME, DISABLE);
-	if (tx_fifo_in_use)
-		rv = MessBuf_Write(&messBuf, m) ? 0 : 1;
-	else{
-		rv = lowlevel_canSend(m);
-		if(!rv){	// si pas de mailbox libre on écrit dans la fifo
-			rv = MessBuf_Write(&messBuf, m) ? 0 : 1;
-			tx_fifo_in_use = 1;		
-		}
-	}
-	if (tx_fifo_in_use)
-  		CAN_ITConfig(CANx, CAN_IT_TME, ENABLE);
-	return rv;
-}
-
 //The driver pass a received CAN message to the stack
 /*
 unsigned char canReceive(Message *m)
@@ -329,7 +226,7 @@ unsigned char canChangeBaudRate_driver( CAN_HANDLE fd, char* baud)
   * @param  None
   * @retval None
   */
-void USB_LP_CAN1_RX0_IRQHandler(void)
+void CAN1_RX0_IRQHandler(void)
 {
 	int i;
 	CanRxMsg RxMessage = {0};
@@ -358,46 +255,3 @@ void enable_it(void)
 	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
 	CAN_ITConfig(CANx, CAN_IT_FMP0, ENABLE);
 }
-
-/*--------- Jeu de fonctions pour utiliser le buffer de messages CAN ----------*/
-/*=============================================================================*/
-
-void MessBuf_Init(MessBuf_t *pFifo)
-{
-	pFifo->readp=pFifo->writep=pFifo->Buf;
-}
-
-/* Ecriture d'un nouveau message dans le buffer
- * retour :  1 buffer plein
- *           0 ok
- */
-int MessBuf_Write(MessBuf_t *pFifo, Message *pgd)
-{
-	int Space;
-	/* calcul espace restant dans la fifo */
-	Space = (pFifo->writep < pFifo->readp) ? 
-		(pFifo->readp - pFifo->writep -1) : (pFifo->readp - pFifo->writep + (MESS_BUFFER_SIZE-1));
-	if(Space == 0)
-		return 1;
-	*(pFifo->writep)=*pgd;
-	pFifo->writep++;
-	if (pFifo->writep >= (pFifo->Buf + MESS_BUFFER_SIZE ))
-		pFifo->writep = pFifo->Buf;
-	return 0;
-}
-
-/* Lecture d'un  message dans le buffer
- * retour :  1 buffer vide
- *           0 ok
- */
-int MessBuf_Read(MessBuf_t *pFifo, Message *pgd)
-{
-	if (pFifo->readp == pFifo->writep)
-		return 1;
-	*pgd=*(pFifo->readp);
-	pFifo->readp++;
-	if(pFifo->readp >= (pFifo->Buf + MESS_BUFFER_SIZE))
-		pFifo->readp = pFifo->Buf;
-	return 0;
-}
-
