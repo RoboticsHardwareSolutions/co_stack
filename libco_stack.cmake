f4eb96ca2130194fb73426ee292ed668deec5204
DocPillman <doc.pillman@icloud.com>
2020-11-10 19:12:20 +0300

    add .cmake

--- libco_stack.cmake
+++ libco_stack.cmake
@@ -12,7 +12,7 @@ endif ()
 
 
 set(CO_STACK_DIRECTORIES  co_stack/drivers co_stack/include co_stack/src co_stack/ )
-file(GLOB CO_STACK_EXECUTABLE_FILES  "co_stack/*.*" )
+file(GLOB_RECURSE CO_STACK_EXECUTABLE_FILES  "co_stack/include/*.*" "co_stack/src/*.*" )
 
 
 
