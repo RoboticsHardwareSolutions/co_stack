e18f0435e8cc87d7985277478bd3b74739b82d5e
DocPillman <doc.pillman@icloud.com>
2020-11-10 17:03:05 +0300

    add .cmake

---
+++ libco.cmake
@@ -0,0 +1,24 @@
+if (MSVC OR MSYS OR MINGW)
+    add_definitions(-DCO_STACK_FOR_WINDOWS)
+endif ()
+
+if (APPLE)
+    add_definitions(-DCO_STACK_FOR_APPLE)
+endif ()
+
+if (UNIX AND NOT APPLE)
+    add_definitions(-DCO_STACK_FOR_UNIX)
+endif ()
+
+
+set(CO_STACK_DIRECTORIES  co_stack/drivers co_stack/include co_stack/src co_stack/ )
+file(GLOB CO_STACK_EXECUTABLE_FILES  "co_stack/*.*" )
+
+
+
+
+
+
+
+
+
